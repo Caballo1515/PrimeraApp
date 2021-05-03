da74471876f0d45e528a7fe9cfcbac378f3e6282
Claudiavp01 <79403941+Claudiavp01@users.noreply.github.com>
2021-05-03 14:36:33 +0200

    .

--- app/src/main/java/cat/urv/deim/asm/dev_comm/ConditionActivity.java
+++ app/src/main/java/cat/urv/deim/asm/dev_comm/ConditionActivity.java
@@ -3,14 +3,21 @@ package cat.urv.deim.asm.dev_comm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.webkit.WebView;
+import android.webkit.WebSettings;
 import cat.urv.deim.asm.dev_comm.R;
 
 public class ConditionActivity extends AppCompatActivity {
+
+    String url = "https://www.youtube.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.conditions_activity_layout);
+        WebView miVisorWeb = (WebView) findViewById(R.id.web);
+        final WebSettings ajustesVisorWeb = miVisorWeb.getSettings();
+        ajustesVisorWeb.setJavaScriptEnabled(true);
+        miVisorWeb.loadUrl(url);
     }
 }
