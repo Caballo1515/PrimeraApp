c65dfeac408b836c696a559fb8964b36cb4a46d5
Alexandru Kitsenco <alexandru.chitenco@estudiants.urv.cat>
2021-05-04 19:34:34 +0200

    tutorial

--- app/src/main/java/cat/urv/deim/asm/dev_comm/TutorialActivity.java
+++ app/src/main/java/cat/urv/deim/asm/dev_comm/TutorialActivity.java
@@ -1,6 +1,7 @@
 package cat.urv.deim.asm.dev_comm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,11 +13,24 @@ import cat.urv.deim.asm.dev_comm.R;
 import cat.urv.deim.asm.dev_comm.LoginActivity;
 
 public class TutorialActivity extends AppCompatActivity {
+    private View f1, f2, f3, f4;
+    private int contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tutorial_activity_layout);
+
+        f1 = this.findViewById(R.id.fragment2);
+        f1.setVisibility(View.INVISIBLE);
+        f2 = this.findViewById(R.id.fragment3);
+        f2.setVisibility(View.INVISIBLE);
+        f3 = this.findViewById(R.id.fragment4);
+        f3.setVisibility(View.INVISIBLE);
+        f4 = this.findViewById(R.id.fragment5);
+        f4.setVisibility(View.INVISIBLE);
+
+        contador=0;
     }
 
 
@@ -32,5 +46,39 @@ public class TutorialActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        Button tutorial = this.findViewById(R.id.button10);
+
+        tutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contador++;
+                if (contador<=4){
+                    switch (contador){
+                        case 1 :
+                            f1.setVisibility(View.VISIBLE);
+                            break;
+                        case 2 :
+                            f2.setVisibility(View.VISIBLE);
+                            break;
+                        case 3:
+                            f3.setVisibility(View.VISIBLE);
+                            break;
+                        case 4:
+                            f4.setVisibility(View.VISIBLE);
+                        default:
+                            break;
+                    }
+
+
+
+                }else {
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+
     }
 }
